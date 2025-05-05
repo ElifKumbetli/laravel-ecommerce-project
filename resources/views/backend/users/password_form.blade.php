264d73e568cf7f6c19ffded22655f7da2ac8ce7e
ElifKumbetli <elifkumbetli@gmail.com>
2025-05-05 18:06:45 +0300

    Html Component Kullanımı Kısım - 2 Tamamlandı

--- resources/views/backend/users/password_form.blade.php
+++ resources/views/backend/users/password_form.blade.php
@@ -1,38 +1,30 @@
+@extends('backend.shared.backend_theme')
 
-@extends("backend.shared.backend_theme")
-
-@section("title", "Kullanıcı Modülü")
-@section("subtitle", "Şifre Değiştir")
-@section("btn_url", url("/users"))
-@section("btn_label", "Geri Dön")
-@section("btn_icon", "arrow-left")
+@section('title', 'Kullanıcı Modülü')
+@section('subtitle', 'Şifre Değiştir')
+@section('btn_url', url('/users'))
+@section('btn_label', 'Geri Dön')
+@section('btn_icon', 'arrow-left')
 
 @section('content')
-  
+
     <form action="{{ url('/users/' . $user->user_id . '/change-password') }}" method="POST">
         @csrf
         <div class="row">
             <div class="col-lg-6">
-                <label for="password" class="form-label">Şifre Giriniz</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Şifre giriniz">
-                @error("password")
-                    <span class="text-danger">{{ $message }}</span>
-                @enderror
+                <x-input label="Şifre Giriniz" placeholder="Şifre giriniz" field="password" type="password" />
             </div>
             <div class="col-lg-6">
-                <label for="password_confirmation" class="form-label">Şifrenizi tekrar giriniz</label>
-                <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Şifrenizi tekrar giriniz" autocomplete="new-password">
-                @error("password_confirmation")
-                    <span class="text-danger">{{ $message }}</span>
-                @enderror
+                <x-input label="Şifre Tekrarı" placeholder="Şifrenizi tekrar giriniz" field="password_confirmation"
+                    type="password" />
             </div>
         </div>
         <div class="row">
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary mt-2">Kaydet</button>
-          </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary mt-2">Kaydet</button>
+            </div>
         </div>
-      </div>
+        </div>
     </form>
 
 @endsection
--- resources/views/backend/users/update_form.blade.php
+++ resources/views/backend/users/update_form.blade.php
@@ -1,62 +1,43 @@
-@extends("backend.shared.backend_theme")
+@extends('backend.shared.backend_theme')
 
-@section("title", "Kullanıcı Modülü")
-@section("subtitle", "Kullanıcı Güncelle")
-@section("btn_url", url("/users"))
-@section("btn_label", "Geri Dön")
-@section("btn_icon", "arrow-left")
+@section('title', 'Kullanıcı Modülü')
+@section('subtitle', 'Kullanıcı Güncelle')
+@section('btn_url', url('/users'))
+@section('btn_label', 'Geri Dön')
+@section('btn_icon', 'arrow-left')
 
 @section('content')
-      <form action="{{ url('/users/' . $user->user_id) }}" method="POST" autocomplete="off" novalidate>
+    <form action="{{ url('/users/' . $user->user_id) }}" method="POST" autocomplete="off" novalidate>
         @csrf
         @method('PUT')
         <input type="hidden" name="user_id" value="{{ $user->user_id }}">
-    
-          <div class="row">
+
+        <div class="row">
             <div class="col-lg-6">
-            
-              <label for="name" class="form-label">Ad Soyad</label>
-              <input type="text" class="form-control" id="name" name="name" placeholder="Ad soyad giriniz" value="{{old("name",$user->name)}}">
-              @error("name")
-              <span class="text-danger">{{$message}}</span>   
-                          
-               @enderror
+                <x-input label="Ad Soyad" placeholder="Ad soyad giriniz" field="name" value="{{ $user->name }}" />
+
             </div>
+
             <div class="col-lg-6">
-              <label for="email" class="form-label">E posta giriniz</label>
-              <input type="email" class="form-control" id="email" name="email" placeholder="E-posta giriniz" value="{{old("email", $user->email)}}">
-              @error("email")
-              <span class="text-danger">{{$message}}</span>   
-                          
-               @enderror
+                <x-input label="Eposta giriniz" placeholder="Eposta giriniz" field="email" type="email"
+                    value="{{ $user->email }}" />
             </div>
-          </div>
-                
-          <div class="row">
+        </div>
+
+        <div class="row">
             <div class="col-lg-6 mt-4">
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="is_admin" name="is_admin" value="1" {{$user->is_admin == 1 ? "checked" : ""}}>
-                <label class="form-check-label" for="is_admin">
-                  Yetkili Kullanıcı
-                </label>
-              </div>
+                <x-checkbox field="is_admin" label="Yetkili Kullanıcı" checked="{{ $user->is_admin == 1 }}" />
             </div>
-            
+
             <div class="col-lg-6">
-              <div class="form-check mt-4">
-                <input class="form-check-input" type="checkbox" id="is_active" name="is_active" value="1" {{$user->is_active == 1 ? "checked" : ""}}>
-                <label class="form-check-label" for="is_active">
-                  Aktif Kullanıcı
-                </label>
-              </div>
+                <x-checkbox field="is_active" label="Aktif Kullanıcı" checked="{{ $user->is_active == 1 }}" />
             </div>
-          
+
             <div class="row">
-              <div class="col-12">
-                <button type="submit" class="btn btn-success mt-2">Kaydet</button>
-              </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-success mt-2">Kaydet</button>
+                </div>
             </div>
-          </div>
-        </form>
- @endsection
-   
+        </div>
+    </form>
+@endsection
